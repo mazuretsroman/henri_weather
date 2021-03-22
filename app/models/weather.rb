8b17cec98495b26e7ae8bf26c06488863b20f41c
Roman Mazurets <roman.mazurets@syndicode.com>
2021-03-22 17:23:03 +0200

    calculate avg temp

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -4,6 +4,8 @@ class Weather < ApplicationRecord
   belongs_to :location
 
   def temp_avg
+    return if temp_max.nil? || temp_min.nil?
+
     [temp_max, temp_min].sum / 2
   end
 end
--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -8,10 +8,20 @@ RSpec.describe Weather, type: :model do
   end
 
   describe '.temp_avg' do
-    it 'returns average temperature' do
-      weather = create(:weather, temp_min: 0, temp_max: 10)
+    context 'when temp_min and temp_max present' do
+      it 'returns average temperature' do
+        weather = create(:weather, temp_min: 0, temp_max: 10)
 
-      expect(weather.temp_avg).to eq(5)
+        expect(weather.temp_avg).to eq(5)
+      end
+    end
+
+    context 'when temp_min and/or temp_max nil' do
+      it 'returns nil' do
+        weather = create(:weather, temp_min: nil, temp_max: 10)
+
+        expect(weather.temp_avg).to eq(nil)
+      end
     end
   end
 end
