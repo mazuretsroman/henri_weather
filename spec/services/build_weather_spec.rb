67cb89e634d434911a86a749e80abb5565161e7a
Roman Mazurets <roman.mazurets@syndicode.com>
2021-03-23 09:58:35 +0200

    weather as object instead of model

--- spec/services/build_weather_spec.rb
+++ spec/services/build_weather_spec.rb
@@ -2,22 +2,9 @@
 
 require 'rails_helper'
 
-RSpec.describe SaveWeather do
+RSpec.describe BuildWeather do
   describe '.call' do
     context 'success api call' do
-      it 'saves weather' do
-        location = create(:location)
-        api_response = double(
-          'CallWeatherApi',
-          body: File.read('./spec/factories/example_response.json'),
-          code: 200
-        )
-        service = described_class.new(location: location)
-        allow(service).to receive(:api_response).and_return(api_response)
-
-        expect { service.call }.to change { location.weathers.count }.by(1)
-      end
-
       it 'returns weather' do
         location = create(:location)
         api_response = double(
